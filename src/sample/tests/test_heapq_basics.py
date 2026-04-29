6c31f6977ca965eb2d7c8d4b0aa133f6d12eeb6b
kermyk <kermyk93@gmail.com>
2026-04-29 18:32:50 -0500

    Add first test of basic heapq functionality.

--- src/sample/tests/test_heapq_basics.py
+++ src/sample/tests/test_heapq_basics.py
@@ -1,3 +1,11 @@
 
 import heapq
 
+class Test_heapq_module:
+  def test_canReturnHeapCount(self):
+    pq = []
+    heapq.heappush(pq, (1, "one"))
+    heapq.heappush(pq, (2, "two"))
+    heapq.heappush(pq, (3, "three"))
+    assert 3 == len(pq)
+ 
