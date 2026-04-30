80aca7acd51e1ac19854757be4c4ece1d2b1d59d
kermyk <kermyk93@gmail.com>
2026-04-29 19:16:44 -0500

    Confirm heapq can pop the minimum item and insert a larger item.

--- src/sample/tests/test_heapq_basics.py
+++ src/sample/tests/test_heapq_basics.py
@@ -6,6 +6,7 @@ class Test_heapq_module:
   oneTenth = 1.0/10.
   oneSeventh = 1.0/7.
   oneFifth = 1.0/5.
+  oneHalf = 1.0/2.
 
   def test_canInsertRealNumber(self):
     pq = []
@@ -14,9 +15,9 @@ class Test_heapq_module:
 
   def test_canReturnHeapCount(self):
     pq = []
-    heapq.heappush(pq, (1, "one"))
-    heapq.heappush(pq, (2, "two"))
-    heapq.heappush(pq, (3, "three"))
+    heapq.heappush(pq, (self.oneSeventh, "a seventh"))
+    heapq.heappush(pq, (self.oneTenth, "a tenth"))
+    heapq.heappush(pq, (self.oneFifth, "a fifth"))
     assert 3 == len(pq)
  
   def test_canReturnMinimumValueOfHeap(self):
@@ -25,4 +26,14 @@ class Test_heapq_module:
     heapq.heappush(pq, (self.oneTenth, "a tenth"))
     heapq.heappush(pq, (self.oneFifth, "a fifth"))
     assert self.oneTenth == pq[0][0]
+
+  def test_canRemoveMinimumItemAndInsertSomethingLarger(self):
+    pq = []
+    heapq.heappush(pq, (self.oneSeventh, "a seventh"))
+    heapq.heappush(pq, (self.oneTenth, "a tenth"))
+    heapq.heappush(pq, (self.oneFifth, "a fifth"))
+    min = heapq.heappop(pq)
+    heapq.heappush(pq, (self.oneHalf, "a half"))
+    assert self.oneTenth == min[0] and 3 == len(pq) and self.oneSeventh == pq[0][0]
+
 
