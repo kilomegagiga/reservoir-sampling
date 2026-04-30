351914bda8032465a555691b82cc6ee7f3f57ace
kermyk <kermyk93@gmail.com>
2026-04-29 19:05:06 -0500

    Confirm that real values work with heapq.

--- src/sample/tests/test_heapq_basics.py
+++ src/sample/tests/test_heapq_basics.py
@@ -2,6 +2,14 @@
 import heapq
 
 class Test_heapq_module:
+
+  oneSeventh = 1.0/7.
+
+  def test_canInsertRealNumber(self):
+    pq = []
+    heapq.heappush(pq, (self.oneSeventh, "a seventh"))
+    assert self.oneSeventh == pq[0][0]
+
   def test_canReturnHeapCount(self):
     pq = []
     heapq.heappush(pq, (1, "one"))
