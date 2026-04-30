3738604d31f21b8f6c05b9c80861baef7725c70a
kermyk <kermyk93@gmail.com>
2026-04-30 11:51:37 -0500

    Add initial implementation of psuedocode and a test to check for syntax errors.

--- src/sample/sample.py
+++ src/sample/sample.py
@@ -36,4 +36,31 @@
 ## is provided by the heapq module.
 import heapq
 
+import random
+
+class WeightedSample:
+  ## Select a weighted sample using A-Res reservoir sampling
+  items = []
+  k = 0  ## size of sample to select
+  pq = []  ## a priority queue ultimately holding the sample
+  
+  def __init__(self, weightedItemsI, kI):
+    ## a weightedItem is a tuple of form (weight, rawItem)
+    self.k = kI
+    self.items = weightedItemsI
+
+  def extract(self):
+    self.pq = []
+
+    for item in self.items:
+      r = random.random() ** (1/item[0])
+      if len(self.pq) < self.k:
+        heapq.heappush(self.pq, (r, item[1]))
+      else:
+        ## keep k items with largest associated keys
+        if r > self.pq[0][0]:
+          heapq.heappop(self.pq)
+          heapq.heappush(self.pq, (r, item[1]))
+    ## self.pq contains the result
+    return self.pq
 
